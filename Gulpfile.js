57083aa364414b370795620fc4606b2ac883e420
Ben Adams <adamsben@google.com>
2017-04-21 12:39:44 -0700

    initial setup

---
+++ Gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var bs = require('browser-sync').create();
+
+gulp.task('browser-sync', ['sass'], function() {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('sass', function () {
+    return gulp.src('scss/*.scss')
+                .pipe(sass())
+                .pipe(gulp.dest('css'))
+                .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', bs.reload);
+});
