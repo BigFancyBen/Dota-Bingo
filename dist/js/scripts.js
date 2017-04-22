640ea7ef9eb0ebfcf82d869137536af7eb9fa0cb
Ben Adams <adamsben@google.com>
2017-04-21 18:08:44 -0700

    schemes needed a https then http

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,11 +1,11 @@
+var player_id = 49697106;
 var request = new XMLHttpRequest();
-request.open('GET', 'https://api.opendota.com/api/players/49697106/wordcloud?limit=100', true);
+request.open('GET', 'https://api.opendota.com/api/players/'+ player_id +'/wordcloud?limit=100', true);
 
 request.onload = function() {
   if (this.status >= 200 && this.status < 400) {
     // Success!
     var data = JSON.parse(this.response);
-    console.log(data);
 
     if (data.my_word_counts.gg > 70 ){
       var d = document.getElementById("ggout");
@@ -23,8 +23,15 @@ request.onload = function() {
   }
 };
 
-request.onerror = function() {
-  // There was a connection error of some sort
-};
+var request2 = new XMLHttpRequest();
+request2.open('GET', 'https://api.opendota.com/api/players/'+ player_id +'/pros?limit=100&win=1', true);
+
+request2.onload = function() {
+  if (this.status >= 200 && this.status < 400) {
+    // Success!
+    var data = JSON.parse(this.response);
+  }
+}
 
 request.send();
+request2.send();
