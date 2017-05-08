6a70be2fa7b609ddebc928ffdc04bddc6afceedd
Ben Adams <adamsben@google.com>
2017-05-08 15:34:13 -0700

    move some js around so its a bit more logically sorted

---
+++ js/cardlogic.js
@@ -0,0 +1,53 @@
+function checkSquares() {
+  let cardArray = [cardStrHeroes,cardKillStreaks,cardQuickBlink,cardFarmFail,
+      cardMoneyHand,cardRandomWin,cardThrowGame,cardSpeedrun,cardLongGame,
+      cardSalty,cardDrowStrat,cardSoloSupport,cardObjectiveGamer];
+
+  let randomCard = shuffle(cardArray, player_id);
+  let currentCard = 0;
+  matchHeroes();
+
+  while(randomCard[currentCard]){
+    for(let y = 1; y<6;y++){
+      for(let x = 1; x<6;x++){
+        if(!(x==3 && y==3)){
+          if(typeof randomCard[currentCard] != 'undefined'){
+            randomCard[currentCard](`card-${y}-${x}`);
+            currentCard++;
+          }
+        }
+      }
+    }
+  }
+}
+
+function addSquare(card_id, card_name, card_tooltip, card_completed) {
+  let card = `<div class='tooltip'>${card_tooltip}</div><span>${card_name}</span>`;
+  let d = document.getElementById(card_id)
+  d.innerHTML = card;
+
+  if(card_completed) {
+    d.className += " has-square";
+  } else {
+    d.classList.remove("has-square");
+  }
+}
+
+function ifExists (maybeExists){
+  if(maybeExists){
+    return maybeExists;
+  }else{
+    return 0;
+  }
+}
+
+function matchHeroes () {
+  for(let i=0; i<10; i++){
+    let cur_hero = data.players[i].hero_id;
+    for(let j=0; j<heroes.length;j++){
+      if(heroes[j].id == cur_hero){
+        current_heroes.push(heroes[j]);
+      }
+    }
+  }
+}
--- js/cards.js
+++ js/cards.js
@@ -1,26 +1,3 @@
-function checkSquares() {
-  let cardArray = [cardStrHeroes,cardKillStreaks,cardQuickBlink,cardFarmFail,
-      cardMoneyHand,cardRandomWin,cardThrowGame,cardSpeedrun,cardLongGame,
-      cardSalty,cardDrowStrat,cardSoloSupport];
-
-  let randomCard = shuffle(cardArray, player_id);
-  let currentCard = 0;
-  matchHeroes();
-
-  while(randomCard[currentCard]){
-    for(let y = 1; y<6;y++){
-      for(let x = 1; x<6;x++){
-        if(!(x==3 && y==3)){
-          if(typeof randomCard[currentCard] != 'undefined'){
-            randomCard[currentCard](`card-${y}-${x}`);
-            currentCard++;
-          }
-        }
-      }
-    }
-  }
-}
-
 function cardKillStreaks(card_id){
   let card_name = "Killing Frenzy";
   let card_tooltip ="10+ kill streak";
@@ -197,32 +174,9 @@ function cardDrowStrat(card_id){
   addSquare(card_id, card_name, card_tooltip, ranged_heroes >= 3);
 }
 
-function ifExists (maybeExists){
-  if(maybeExists){
-    return maybeExists;
-  }else{
-    return 0;
-  }
-}
+function cardObjectiveGamer(card_id){
+  let card_name = "Wolves need no armor";
+  let card_tooltip ="More building damage than hero damage";
 
-function matchHeroes () {
-  for(let i=0; i<10; i++){
-    let cur_hero = data.players[i].hero_id;
-    for(let j=0; j<heroes.length;j++){
-      if(heroes[j].id == cur_hero){
-        current_heroes.push(heroes[j]);
-      }
-    }
-  }
-}
-function addSquare(card_id, card_name, card_tooltip, card_completed) {
-  let card = `<div class='tooltip'>${card_tooltip}</div><span>${card_name}</span>`;
-  let d = document.getElementById(card_id)
-  d.innerHTML = card;
-
-  if(card_completed) {
-    d.className += " has-square";
-  } else {
-    d.classList.remove("has-square");
-  }
+  addSquare(card_id, card_name, card_tooltip, ifExists(player.tower_damage) > ifExists(player.hero_damage));
 }
